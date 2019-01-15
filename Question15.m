1c73323ca13dc600c04dab0343ffe75aa3428314
NicholasGay <38671804+NicholasGay@users.noreply.github.com>
2019-01-15 20:18:21 +0100

    Add files via upload

--- Question15.m
+++ Question15.m
@@ -1,9 +1,29 @@
+close all
 clear all
 clc
 load('exampleMAC')
 S = 20;
-Ptx = [0;0];
-P = 10.^(Ptx/10);
-[R] = ParetoBound(H,P,S);
+PtxdB = [-10, 0, 10];
+Ptx = 10.^(PtxdB/10);
+P(1,:) = Ptx;
+P(2,:) = Ptx;
+fig  = figure;
+hold on
+for i = 1:size(P,2)
+[R] = ParetoBound(H,P(:,i),S);
 
-[fig] = plotRegionMAC(R);
+%% Plotting rectangular for reference not needed actually
+
+rectangle = [R(:,1),[R(1,end);R(2,1)],R(:,end)];
+rectplot = plot(rectangle(1,:),rectangle(2,:),'--k','HandleVisibility','off');
+triangle = [R(:,1),R(:,end)];
+triplot = plot(triangle(1,:),triangle(2,:),':k','HandleVisibility','off');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fig = plotRegionMAC(R,fig);
+
+
+end
+legend('-10 dB','0 dB','10 dB')
+xlabel('R_1')
+ylabel('R_2')
